a46f3906b03e83bddcd6b7e74208cf71e787328d
Mario Matheus <mariomatheu@gmail.com>
2017-11-20 10:37:29 -0300

    Adicionando jsons...

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "hello-vapor",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/json.git", majorVersion: 2)
     ]
 )
--- Sources/hello-vapor/main.swift
+++ Sources/hello-vapor/main.swift
@@ -1,4 +1,5 @@
 import Vapor
+import JSON
 
 let drop = try Droplet()
 
@@ -6,8 +7,26 @@ drop.get("hello-vapor") { req in
   return "Hello Vapor"
 }
 
-drop.get("html") { req in
+drop.get("gethtml") { req in
     return try drop.view.make("index.html")
+}
+
+drop.get("lversion") { req in
+    var json = JSON()
+    try json.set("version", 1.0)
+    try json.set("lv-learn", 1.1)
+    return json
+}
+
+drop.post("sendjson") { req in
+    guard let nome = req.json?["nome"]?.string else {
+        throw Abort(.badRequest)
+    }
+    guard let pai = req.json?["pai"]?.string else {
+        throw Abort(.badRequest)
+    }
+
+    return "Olá, \(nome), filho de \(pai)!"
 }
 
 try drop.run()
